042d571d84c8386f33ec8ab716aea8ae89697f15
YPAN <56719872+ywpan@users.noreply.github.com>
2020-04-23 11:45:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+// 2020年4月23日
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "Hello buddy",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "It is your birthiday",  // 同上...
+        "Today!",
+        "The 27th Birthday",
+        "Sounds old already",
+        "Last year we went to watch",
+        "Toy Story 4",
+        "and have cake",
+        "Hope you will enjoy today",
+        "生日快乐Calum！",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -30,12 +29,12 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        turn_on: "Start！",
+        play: "Music",
+        bannar_coming: "Some Fasti theme",
+        balloons_flying: "Missing something",
+        cake_fadein: "Cake？",
+        light_candle: "Candle？",
         wish_message: "生日快乐",
         story: "A MESSAGE FOR YOU",
     }
